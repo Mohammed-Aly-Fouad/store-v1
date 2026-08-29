217f89c887bb3a9aaad9dabddfbd0737f1e7f59e
Mohammed_maktap_Noor <mohammedalyfouad@gmail.com>
2026-08-29 09:17:00 +0300

    Starting over

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -10,7 +10,6 @@ pub fn router() -> Router<AppState> {
     Router::new()
         .nest("/brands", brand::router())
         .nest("/categories", category::router())
-        .nest("/test", category2::router())
 
       
 }
